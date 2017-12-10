a592b1ce8365ff9649eef12122cb785663fbeda4
Jan Pluskal <ipluskal@fit.vutbr.cz>
2017-12-10 20:45:40 +0100

    Add Theme example.

---
+++ Examples/WPF/Theme/WPFThemeDemo.DesktopClient/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="App.xaml.cs" company="DCOM Engineering, LLC">
+//      Copyright (c) DCOM Engineering, LLC.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace WPFThemeDemo.DesktopClient
+{
+    using System.Windows;
+
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Examples/WPF/Theme/WPFThemeDemo.DesktopClient/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="MainWindow.xaml.cs" company="DCOM Engineering, LLC">
+//      Copyright (c) DCOM Engineering, LLC.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace WPFThemeDemo.DesktopClient
+{
+    using System.Windows;
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Button1.IsEnabled = true;
+        }
+    }
+}
